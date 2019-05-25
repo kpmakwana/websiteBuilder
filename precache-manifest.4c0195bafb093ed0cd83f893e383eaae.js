3b89995b4da0d6c8e0ff4ec14989605e70f5979a
kaushik <makwana.kaushik98@gmail.com>
2019-05-25 20:00:25 +0530

    Updates

---
+++ precache-manifest.4c0195bafb093ed0cd83f893e383eaae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb36a14e85805c6b0325",
+    "url": "/websiteBuilder/static/js/runtime~main.fb36a14e.js"
+  },
+  {
+    "revision": "85156f66c2ee8f6ef800",
+    "url": "/websiteBuilder/static/js/main.85156f66.chunk.js"
+  },
+  {
+    "revision": "a6eb33abc372cd9759d2",
+    "url": "/websiteBuilder/static/js/2.a6eb33ab.chunk.js"
+  },
+  {
+    "revision": "85156f66c2ee8f6ef800",
+    "url": "/websiteBuilder/static/css/main.baa50f25.chunk.css"
+  },
+  {
+    "revision": "a6eb33abc372cd9759d2",
+    "url": "/websiteBuilder/static/css/2.7e517a0a.chunk.css"
+  },
+  {
+    "revision": "2d2e1e6d39be64634ae44e12ee093d48",
+    "url": "/websiteBuilder/index.html"
+  }
+];
